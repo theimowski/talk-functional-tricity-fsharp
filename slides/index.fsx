f4ac53907b8ea0ebdc36388a3c6e96dc02e13656
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-08-08 07:33:15 +0200

    update TOC

--- slides/index.fsx
+++ slides/index.fsx
@@ -22,11 +22,50 @@ http://theimowski.com/
 ## Agenda
 
 1. Brief history
-2. Evolution
-3. Comparison to Scala
-4. Best features
-5. Dispelling the myth?
+2. Comparison with Scala
+3. Greatest features
+4. Evolution of the language
+5. Using F# at work
 
 ***
+
+## Brief history
+
+* Microsoft Research initiative
+* Biggest influencers
+
+***
+
+## Comparison with Scala
+
+* Semantics, Syntax
+* Main assumptions
+
+
+***
+
+## Greatest features
+
+* Type inference
+* Type providers
+* Miscellaneous (Active patterns, UOM)
+
+***
+
+## Evolution of the language
+
+* Community-driven
+* X-Plat support
+
+***
+
+## Using F# at work
+
+* Safe ways for introducing new language
+* Practical example
+
+***
+
+## Q&A
 
 *)
